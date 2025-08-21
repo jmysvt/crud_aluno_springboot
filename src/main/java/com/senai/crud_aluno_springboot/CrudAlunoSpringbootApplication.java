ab75757491dbcea2066e8c5b3656c56a2f09152b
Pedro <pedrosantos471572@gmail.com>
2025-08-21 08:38:35 -0300

    Initial commit

---
+++ src/main/java/com/senai/crud_aluno_springboot/CrudAlunoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.senai.crud_aluno_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAlunoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAlunoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/crud_aluno_springboot/CrudAlunoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.crud_aluno_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAlunoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
